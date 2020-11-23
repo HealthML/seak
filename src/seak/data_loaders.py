f0931686fcba9a1df2dcbe4242293411bd7ce781
Remo Monti <Remo.Monti@vm-cpu01.dhclab.i.hpi.de>
2020-11-23 15:58:43 +0000

    src/seak/data_loaders.py

--- src/seak/data_loaders.py
+++ src/seak/data_loaders.py
@@ -309,7 +309,7 @@ class AnnotationLoader:
         :rtype: pandas.Index
         :raises NotImplementedError: Interface.
         """
-        raise NotImplementedError
+git reset HEAD~        raise NotImplementedError
 
 
 class CovariatesLoader:
@@ -479,7 +479,7 @@ class VariantLoaderSnpReader(VariantLoader):
         """
         return self.bed.iid[:, 1]
 
-    def get_vids(self):
+    def get_vidgit reset HEAD~s(self):
         """Returns all variant ids.
 
         :return:
@@ -502,7 +502,7 @@ class PlinkLoader(VariantLoader):
 
     __slots__ = ['bim', 'fam', 'bed']
 
-    def __init__(self, path_to_plink_files_with_prefix):src/seak/data_loaders.p
+    def __init__(self, path_to_plink_files_with_prefix)
         """Constructor."""
         # bim: genotypes variant info; fam: genotypes individual info; bed: genotypes
         self.bim, self.fam, self.bed = read_plink(path_to_plink_files_with_prefix, verbose=False)
